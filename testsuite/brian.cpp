ad8b154418623b29778a1cdd686d8d7db01a4e51
Brian Malloy <malloy@clemson.edu>
2012-05-03 15:33:01 -0400

    Added some functions to my testcase

--- testsuite/brian.cpp
+++ testsuite/brian.cpp
@@ -8,9 +8,19 @@ namespace NamespaceA{
 
 class A{
 public:
-  void functionByBrian();
+  A() : integer(0), floatingPoint(0.0) {}
+  A(int n, float x) : integer(n), floatingPoint(x) {}
+  int getInt() const;
+  float getFloat() const { return integer; }
+  void useNamespace() const;
+private:
+  int integer;
+  int floatingPoint;
 };
-void A::functionByBrian() { int x; };
+int A::getInt() const { return integer; };
+void A::useNamespace() const {
+  NamespaceA::functionInNamespaceA();
+}
 
 int getX() {
   return 1138;
@@ -19,5 +29,8 @@ int getX() {
 int main(int argc, char *argv[])
 {
    std::cout << "Hello, World!" << std::endl;
+   A a;
+   int i = a.getInt();
+   int x = a.getFloat();
    return EXIT_SUCCESS;
 }
