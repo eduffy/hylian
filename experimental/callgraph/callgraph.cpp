3e0e3a38106340922fd0ed66453ed7a862229350
Brian Malloy <malloy@clemson.edu>
2012-06-20 09:36:39 -0400

    Building string to represent caller. Need to add const qualifier and then it will done!

--- experimental/callgraph/callgraph.cpp
+++ experimental/callgraph/callgraph.cpp
@@ -14,23 +14,37 @@ void HylianASTConsumer::HandleTopLevelDecl(clang::DeclGroupRef declGroup)
       const clang::Decl *decl = clang::dyn_cast<clang::Decl>(*i);
       if(clang::FunctionDecl::classof(decl)) {
          const clang::FunctionDecl *fun = 
-                 clang::dyn_cast<const clang::FunctionDecl>(decl);
-         cout << decl->getDeclKindName()
-              << ": "
-              << "Returns " << fun->getResultType().getAsString()
-              << ", Name: "
-              << fun->getQualifiedNameAsString()
-              << std::endl;
+               clang::dyn_cast<const clang::FunctionDecl>(decl);
+         std::string caller;
+         caller += fun->getResultType().getAsString();
+         caller += " "+fun->getQualifiedNameAsString();
+         caller+= "(";
+         bool first = true;
          clang::FunctionDecl::param_const_iterator it;
-         cout << "\tParameters: ";
          for (it = fun->param_begin();
               it != fun->param_end();
               ++it) {
-           cout << (*it)->getType().getAsString() << " "
-                << (*it)->getIdentifier()->getName().str()
-                << ",\t";
+           if (!first) caller += ",";
+           caller += (*it)->getType().getAsString() + " ";
+           if ( (*it)->getIdentifier() ) {
+             caller += (*it)->getIdentifier()->getName().str();
+           }
+           first = false;
          }
-         cout << endl;
+         caller += ")";
+         if(clang::CXXMethodDecl::classof(decl)) 
+         {
+           const clang::CXXMethodDecl *fun = 
+                 clang::dyn_cast<const clang::CXXMethodDecl>(decl);
+           clang::ASTContext& context = fun->getParentASTContext();
+           cout << "THIS: " << fun->getThisType(context).getAsString() << endl;
+           if ( fun->getThisType(context).isConstQualified() ) 
+           {
+             caller += " const";
+           }
+         }
+
+         cout << caller << endl;
       }
       else {
         // Usually a Namespace, CXXRecord, Typedef, LinkageSpec, or Enum...
