d8423ee857821de54ed159899f2b1f580b749d6a
Brian Malloy <malloy@clemson.edu>
2012-06-21 10:51:21 -0400

    more complete test case for class methods

---
+++ testsuite/noioBrian.cpp
@@ -0,0 +1,33 @@
+namespace NamespaceA{
+  void functionInNamespaceA() {}
+}
+
+class A{
+public:
+  A() : integer(0), floatingPoint(0.0) {}
+  A(int n, float x) : integer(n), floatingPoint(x) {}
+  A(const A& a) : integer(a.integer), floatingPoint(a.floatingPoint) {}
+  A& operator=(const A& a) {
+    integer = a.integer;
+    floatingPoint = a.floatingPoint;
+    return *this;
+  }
+  int getInt() const;
+  float getFloat() const { return integer; }
+  void useNamespace() const;
+private:
+  int integer;
+  int floatingPoint;
+};
+int A::getInt() const { return integer; };
+void A::useNamespace() const {
+  NamespaceA::functionInNamespaceA();
+}
+
+int getX() { return 1138; }
+
+int main(int argc, char *argv[]) {
+   A a;
+   int i = a.getInt();
+   int x = a.getFloat();
+}
