f89f4fc68fd02be83fe93a1b5141162edb00fe74
Brian Malloy <malloy@clemson.edu>
2012-06-21 10:52:09 -0400

    I've made some progress in comprehension of Clang, but nothing to show for it, yet ....

--- experimental/callgraph/callgraph.cpp
+++ experimental/callgraph/callgraph.cpp
@@ -7,38 +7,51 @@ using  std::cout; using  std::endl;
 // The following Visitor gives all namespace declarations.
 void HylianASTConsumer::HandleTopLevelDecl(clang::DeclGroupRef declGroup)
 {
-   for(clang::DeclGroupRef::iterator i = declGroup.begin(); 
-                                     i != declGroup.end(); 
-                                     ++i)
+   for(clang::DeclGroupRef::iterator declGroupIterate = declGroup.begin(); 
+                                     declGroupIterate != declGroup.end(); 
+                                     ++declGroupIterate)
    {
-      const clang::Decl *decl = clang::dyn_cast<clang::Decl>(*i);
+      const clang::Decl *decl = 
+            clang::dyn_cast<clang::Decl>(*declGroupIterate);
       if(clang::FunctionDecl::classof(decl)) {
-         const clang::FunctionDecl *fun = 
+         const clang::FunctionDecl *function = 
                clang::dyn_cast<const clang::FunctionDecl>(decl);
          std::string caller;
-         caller += fun->getResultType().getAsString();
-         caller += " "+fun->getQualifiedNameAsString();
+         caller += function->getResultType().getAsString();
+         caller += " "+function->getQualifiedNameAsString();
          caller+= "(";
          bool first = true;
-         clang::FunctionDecl::param_const_iterator it;
-         for (it = fun->param_begin();
-              it != fun->param_end();
-              ++it) {
+         clang::FunctionDecl::param_const_iterator paramIterate;
+         for (paramIterate = function->param_begin();
+              paramIterate != function->param_end();
+              ++paramIterate) {
            if (!first) caller += ",";
-           caller += (*it)->getType().getAsString() + " ";
-           if ( (*it)->getIdentifier() ) {
-             caller += (*it)->getIdentifier()->getName().str();
+           caller += (*paramIterate)->getType().getAsString() + " ";
+           if ( (*paramIterate)->getIdentifier() ) {
+             caller += (*paramIterate)->getIdentifier()->getName().str();
            }
            first = false;
          }
          caller += ")";
          if(clang::CXXMethodDecl::classof(decl)) 
          {
-           const clang::CXXMethodDecl *fun = 
+           const clang::CXXMethodDecl *method = 
                  clang::dyn_cast<const clang::CXXMethodDecl>(decl);
-           clang::ASTContext& context = fun->getParentASTContext();
-           cout << "THIS: " << fun->getThisType(context).getAsString() << endl;
-           if ( fun->getThisType(context).isConstQualified() ) 
+           clang::ASTContext& context = method->getParentASTContext();
+           cout << "THIS: " << method->getThisType(context).getAsString() 
+                << endl;
+           if ( method->isCopyAssignmentOperator() ) 
+           {
+             cout<<"FUNCTION IS overloaded Assignment!\n";
+           }
+           // The next function seems to be implemented incorrectly;
+           // i.e., clang marks it false, but doesn't mark it true?
+           if ( !method->hasInlineBody() ) 
+           {
+             cout << "FUNCTION " << function->getQualifiedNameAsString()
+                  << " IS not INLINED!\n";
+           }
+           if ( method->getThisType(context).isConstQualified() ) 
            {
              caller += " const";
            }
