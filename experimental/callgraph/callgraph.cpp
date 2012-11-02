cbf935e7206be413e9dfe2cec7d79acb08b4fc4a
Brian Malloy <malloy@clemson.edu>
2012-11-02 09:02:47 -0400

    Recognizing outer declarations, including namespace and class

--- experimental/callgraph/callgraph.cpp
+++ experimental/callgraph/callgraph.cpp
@@ -4,66 +4,78 @@
 using  std::cout; using  std::endl;
 #include "callgraph.h"
 
+void processFunction(const clang::Decl *decl) {
+  const clang::FunctionDecl *function = 
+        clang::dyn_cast<const clang::FunctionDecl>(decl);
+  std::string caller;
+  cout << "Function: ";
+  caller += function->getResultType().getAsString();
+  caller += " "+function->getQualifiedNameAsString();
+  caller+= "(";
+  bool first = true;
+  clang::FunctionDecl::param_const_iterator paramIterate;
+  for (paramIterate = function->param_begin();
+       paramIterate != function->param_end();
+       ++paramIterate) {
+    if (!first) caller += ",";
+    caller += (*paramIterate)->getType().getAsString() + " ";
+    if ( (*paramIterate)->getIdentifier() ) {
+      caller += (*paramIterate)->getIdentifier()->getName().str();
+    }
+    first = false;
+  }
+  caller += ")";
+  if(clang::CXXMethodDecl::classof(decl)) 
+  {
+    const clang::CXXMethodDecl *method = 
+          clang::dyn_cast<const clang::CXXMethodDecl>(decl);
+    clang::ASTContext& context = method->getParentASTContext();
+    if ( method->isCopyAssignmentOperator() ) 
+    {
+      cout<<"FUNCTION IS overloaded Assignment!\n";
+    }
+    // The next function seems to be implemented incorrectly;
+    // i.e., clang marks it false, but doesn't mark it true?
+    if ( method->hasInlineBody() ) 
+    {
+      cout << "FUNCTION " << function->getQualifiedNameAsString()
+           << " IS not INLINED!\n";
+    }
+    if ( method->getThisType(context).isConstQualified() ) 
+    {
+      caller += " const";
+    }
+  }
+  cout << caller << endl;
+} // end of processFunction
+
 // The following Visitor gives all namespace declarations.
 void HylianASTConsumer::HandleTopLevelDecl(clang::DeclGroupRef declGroup)
 {
-   for(clang::DeclGroupRef::iterator declGroupIterate = declGroup.begin(); 
-                                     declGroupIterate != declGroup.end(); 
-                                     ++declGroupIterate)
+   for(clang::DeclGroupRef::iterator 
+       declGroupIterate = declGroup.begin(); 
+       declGroupIterate != declGroup.end(); 
+       ++declGroupIterate)
    {
       const clang::Decl *decl = 
             clang::dyn_cast<clang::Decl>(*declGroupIterate);
       if(clang::FunctionDecl::classof(decl)) {
-         const clang::FunctionDecl *function = 
-               clang::dyn_cast<const clang::FunctionDecl>(decl);
-         std::string caller;
-         cout << "Function: ";
-         caller += function->getResultType().getAsString();
-         caller += " "+function->getQualifiedNameAsString();
-         caller+= "(";
-         bool first = true;
-         clang::FunctionDecl::param_const_iterator paramIterate;
-         for (paramIterate = function->param_begin();
-              paramIterate != function->param_end();
-              ++paramIterate) {
-           if (!first) caller += ",";
-           caller += (*paramIterate)->getType().getAsString() + " ";
-           if ( (*paramIterate)->getIdentifier() ) {
-             caller += (*paramIterate)->getIdentifier()->getName().str();
-           }
-           first = false;
-         }
-         caller += ")";
-         if(clang::CXXMethodDecl::classof(decl)) 
-         {
-           const clang::CXXMethodDecl *method = 
-                 clang::dyn_cast<const clang::CXXMethodDecl>(decl);
-           clang::ASTContext& context = method->getParentASTContext();
-           cout << "THIS: " << method->getThisType(context).getAsString() 
-                << endl;
-           if ( method->isCopyAssignmentOperator() ) 
-           {
-             cout<<"FUNCTION IS overloaded Assignment!\n";
-           }
-           // The next function seems to be implemented incorrectly;
-           // i.e., clang marks it false, but doesn't mark it true?
-           if ( method->hasInlineBody() ) 
-           {
-             cout << "FUNCTION " << function->getQualifiedNameAsString()
-                  << " IS not INLINED!\n";
-           }
-           if ( method->getThisType(context).isConstQualified() ) 
-           {
-             caller += " const";
-           }
-         }
-
-         cout << caller << endl;
+        // This is a function in the global anonymous namespace:
+        processFunction(decl);
       }
       else {
         // A Namespace, CXXRecord, Typedef, LinkageSpec, Enum, or ...
-        //cout << "Construct in global namespace, other than a fn: " 
-             //<< decl->getDeclKindName() << endl;
+        //if ( decl->isNamespace() ) {
+        if ( strcmp(decl->getDeclKindName(), "CXXRecord") == 0 ) {
+  const clang::CXXRecordDecl *thisClass = 
+        clang::dyn_cast<const clang::CXXRecordDecl>(decl);             
+          cout << "Class: " << thisClass->getNameAsString() << endl;
+        }
+        if ( strcmp(decl->getDeclKindName(), "Namespace") == 0 ) {
+  const clang::NamespaceDecl *space = 
+        clang::dyn_cast<const clang::NamespaceDecl>(decl);             
+          cout << "Namespace: " << space->getNameAsString() << endl;
+        }
       }
    }
 }
