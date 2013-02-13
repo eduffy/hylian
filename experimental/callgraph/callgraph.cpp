3f10e42030ad6b769b493ca40a1edaee1f5b0854
Edward Duffy <eduffy@clemson.edu>
2013-02-13 09:21:33 -0500

    make spacing/whitespace consistent

--- experimental/callgraph/callgraph.cpp
+++ experimental/callgraph/callgraph.cpp
@@ -3,54 +3,57 @@
 #include <iostream>
 #include "callgraph.h"
 
-void HylianASTConsumer::writeCallgraph() {
+void HylianASTConsumer::writeCallgraph()
+{
    graphWriter.nodeMap("label", nodeLabels);
    graphWriter.run();
 }
 
-void HylianASTConsumer::processFunction(const clang::Decl *decl) {
-  const clang::FunctionDecl *function = 
-        clang::dyn_cast<const clang::FunctionDecl>(decl);
-  std::string funDecl;
-  funDecl += function->getResultType().getAsString();
-  funDecl += " "+function->getQualifiedNameAsString();
-  funDecl+= "(";
-  bool first = true;
-  clang::FunctionDecl::param_const_iterator paramIterate;
-  for (paramIterate = function->param_begin();
-       paramIterate != function->param_end();
-       ++paramIterate) {
-    if (!first) funDecl += ",";
-    funDecl += (*paramIterate)->getType().getAsString() + " ";
-    if ( (*paramIterate)->getIdentifier() ) {
-      funDecl += (*paramIterate)->getIdentifier()->getName().str();
-    }
-    first = false;
-  }
-  funDecl += ")";
-  if(clang::CXXMethodDecl::classof(decl)) 
-  {
-    const clang::CXXMethodDecl *method = 
-          clang::dyn_cast<const clang::CXXMethodDecl>(decl);
-    clang::ASTContext& context = method->getParentASTContext();
-    if ( method->isCopyAssignmentOperator() ) 
-    {
-      std::cout << "FUNCTION IS overloaded Assignment!\n";
-    }
-    // The next function seems to be implemented incorrectly;
-    // i.e., clang marks it false, but doesn't mark it true?
-    if ( method->hasInlineBody() ) 
-    {
-      std::cout << "FUNCTION " << function->getQualifiedNameAsString()
-           << " IS not INLINED!\n";
-    }
-    if ( method->getThisType(context).isConstQualified() ) 
-    {
-      funDecl += " const";
-    }
-  }
-  lemon::ListDigraph::Node funcNode = graph.addNode();
-  nodeLabels[funcNode] = funDecl;
+void HylianASTConsumer::processFunction(const clang::Decl *decl)
+{
+   const clang::FunctionDecl *function = 
+      clang::dyn_cast<const clang::FunctionDecl>(decl);
+   std::string funDecl;
+   funDecl += function->getResultType().getAsString();
+   funDecl += " "+function->getQualifiedNameAsString();
+   funDecl+= "(";
+   bool first = true;
+   clang::FunctionDecl::param_const_iterator paramIterate;
+   for (paramIterate = function->param_begin();
+        paramIterate != function->param_end();
+        ++paramIterate)
+   {
+      if (!first) funDecl += ",";
+      funDecl += (*paramIterate)->getType().getAsString() + " ";
+      if((*paramIterate)->getIdentifier()) {
+         funDecl += (*paramIterate)->getIdentifier()->getName().str();
+      }
+      first = false;
+   }
+   funDecl += ")";
+   if(clang::CXXMethodDecl::classof(decl)) 
+   {
+      const clang::CXXMethodDecl *method = 
+         clang::dyn_cast<const clang::CXXMethodDecl>(decl);
+      clang::ASTContext &context = method->getParentASTContext();
+      if(method->isCopyAssignmentOperator()) 
+      {
+         std::cout << "FUNCTION IS overloaded Assignment!\n";
+      }
+      // The next function seems to be implemented incorrectly;
+      // i.e., clang marks it false, but doesn't mark it true?
+      if(method->hasInlineBody()) 
+      {
+         std::cout << "FUNCTION " << function->getQualifiedNameAsString()
+                   << " IS not INLINED!\n";
+      }
+      if(method->getThisType(context).isConstQualified()) 
+      {
+         funDecl += " const";
+      }
+   }
+   lemon::ListDigraph::Node funcNode = graph.addNode();
+   nodeLabels[funcNode] = funDecl;
 } // end of processFunction
 
 // The following Visitor gives all namespace declarations.
@@ -71,15 +74,15 @@ bool HylianASTConsumer::HandleTopLevelDecl(clang::DeclGroupRef declGroup)
       else {
         // A Namespace, CXXRecord, Typedef, LinkageSpec, Enum, or ...
         //if ( decl->isNamespace() ) {
-        if ( strcmp(decl->getDeclKindName(), "CXXRecord") == 0 ) {
-  const clang::CXXRecordDecl *thisClass = 
-        clang::dyn_cast<const clang::CXXRecordDecl>(decl);             
-          std::cout << "Class: " << thisClass->getNameAsString() << std::endl;
+        if(strcmp(decl->getDeclKindName(), "CXXRecord") == 0) {
+           const clang::CXXRecordDecl *thisClass = 
+              clang::dyn_cast<const clang::CXXRecordDecl>(decl);             
+           std::cout << "Class: " << thisClass->getNameAsString() << std::endl;
         }
-        if ( strcmp(decl->getDeclKindName(), "Namespace") == 0 ) {
-  const clang::NamespaceDecl *space = 
-        clang::dyn_cast<const clang::NamespaceDecl>(decl);             
-          std::cout << "Namespace: " << space->getNameAsString() << std::endl;
+        if(strcmp(decl->getDeclKindName(), "Namespace") == 0) {
+           const clang::NamespaceDecl *space = 
+              clang::dyn_cast<const clang::NamespaceDecl>(decl);             
+           std::cout << "Namespace: " << space->getNameAsString() << std::endl;
         }
       }
    }
