f529ff7fdded27f6931194aabde8b8128af2a58b
Edward Duffy <eduffy@clemson.edu>
2013-02-15 07:40:24 -0500

    added example calls to a variadic function, and a static method

--- testsuite/brian.cpp
+++ testsuite/brian.cpp
@@ -1,5 +1,6 @@
 
 #include <cstdlib>
+#include <cstdio>
 #include <iostream>
 
 namespace NamespaceA{
@@ -13,6 +14,7 @@ public:
   int getInt() const;
   float getFloat() const { return integer; }
   void useNamespace() const;
+  static bool isEven(int number) { return number%2==0; }
 private:
   int integer;
   int floatingPoint;
@@ -29,8 +31,10 @@ int getX(int) {
 int main(int argc, char *argv[])
 {
    std::cout << "Hello, World!" << std::endl;
+   printf("Hello, World\n");
    A a;
    int i = a.getInt();
    int x = a.getFloat();
+   int e = A::isEven(10);
    return EXIT_SUCCESS;
 }
