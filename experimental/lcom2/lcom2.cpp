533813d9821d19f61031c2e373f245fd0b8c6b01
Brian Malloy <malloy@clemson.edu>
2013-11-14 09:23:34 -0500

    Made changes to find class members.

--- experimental/lcom2/lcom2.cpp
+++ experimental/lcom2/lcom2.cpp
@@ -41,87 +41,45 @@ std::string LCOM2Visitor::getQualifiedName(const clang::Decl *decl)
    return result;
 }
 
+/*
 bool LCOM2Visitor::TraverseFunctionDecl(clang::FunctionDecl *decl)
 {
    complexity = 1;
-   if(decl->getBody())
-      TraverseStmt(decl->getBody());
 
    std::string qname = getQualifiedName(decl);
    printf("%5d |  %s\n", complexity, qname.c_str());
 
    complexity = 0;
    return true;
+}*/
+
+bool LCOM2Visitor::TraverseMemberExpr(clang::MemberExpr *expr)
+{
+  clang::ValueDecl *v = expr->getMemberDecl();
+  std::cout << "member expression: " << v->getQualifiedNameAsString() << std::endl;
+  return true;
 }
 
 bool LCOM2Visitor::TraverseCXXMethodDecl(clang::CXXMethodDecl *decl)
 {
-   return TraverseFunctionDecl(decl);
+   if(decl->getBody())
+      TraverseStmt(decl->getBody());
+   return true;
 }
 
 bool LCOM2Visitor::TraverseCXXConversionDecl(clang::CXXConversionDecl *decl)
 {
-   return TraverseFunctionDecl(decl);
+   return TraverseCXXMethodDecl(decl);
 }
 
 bool LCOM2Visitor::TraverseCXXDestructorDecl(clang::CXXDestructorDecl *decl)
 {
-   return TraverseFunctionDecl(decl);
+   return TraverseCXXMethodDecl(decl);
 }
 
 bool LCOM2Visitor::TraverseCXXConstructorDecl(clang::CXXConstructorDecl *decl)
 {
-   return TraverseFunctionDecl(decl);
+   return TraverseCXXMethodDecl(decl);
 }
 
-bool LCOM2Visitor::TraverseIfStmt(clang::IfStmt *stmt)
-{
-   assert(complexity > 0);
-   ++complexity;
-   TraverseStmt(stmt->getThen());
-   if(stmt->getElse() != NULL) {
-      if(!clang::IfStmt::classof(stmt->getElse())) {
-         //  Dont' count "else if" as two blocks
-         ++complexity;
-      }
-      TraverseStmt(stmt->getElse());
-   }
-   return true;
-}
-
-bool LCOM2Visitor::TraverseCaseStmt(clang::CaseStmt *stmt)
-{
-   assert(complexity > 0);
-   ++complexity;
-   if(stmt->getSubStmt())
-      TraverseStmt(stmt->getSubStmt());
-   return true;
-}
-
-bool LCOM2Visitor::TraverseWhileStmt(clang::WhileStmt *stmt)
-{
-   assert(complexity > 0);
-   ++complexity;
-   if(stmt->getBody())
-      TraverseStmt(stmt->getBody());
-   return true;
-}
-
-bool LCOM2Visitor::TraverseDoStmt(clang::DoStmt *stmt)
-{
-   assert(complexity > 0);
-   ++complexity;
-   if(stmt->getBody())
-      TraverseStmt(stmt->getBody());
-   return true;
-}
-
-bool LCOM2Visitor::TraverseForStmt(clang::ForStmt *stmt)
-{
-   assert(complexity > 0);
-   ++complexity;
-   if(stmt->getBody())
-      TraverseStmt(stmt->getBody());
-   return true;
-}
 
--- experimental/lcom2/lcom2.h
+++ experimental/lcom2/lcom2.h
@@ -16,18 +16,12 @@ public:
    LCOM2Visitor(clang::ASTContext *context);
    virtual ~LCOM2Visitor() { }
 
-//   bool VisitFunctionDecl(clang::FunctionDecl *decl);
-   bool TraverseFunctionDecl(clang::FunctionDecl *decl);
    bool TraverseCXXMethodDecl(clang::CXXMethodDecl *decl);
    bool TraverseCXXConversionDecl(clang::CXXConversionDecl *decl);
    bool TraverseCXXDestructorDecl(clang::CXXDestructorDecl *decl);
    bool TraverseCXXConstructorDecl(clang::CXXConstructorDecl *decl);
 
-   bool TraverseIfStmt(clang::IfStmt *stmt);
-   bool TraverseCaseStmt(clang::CaseStmt *stmt);
-   bool TraverseWhileStmt(clang::WhileStmt *stmt);
-   bool TraverseDoStmt(clang::DoStmt *stmt);
-   bool TraverseForStmt(clang::ForStmt *stmt);
+bool TraverseMemberExpr(clang::MemberExpr *expr);
 
 private:
    std::string getQualifiedName(const clang::Decl *);
